44ffbfd213b1da9960e8e2700beda2e85936410f
Vaishnavi wankhade <93776455+wankhadevaish@users.noreply.github.com>
2021-11-27 20:18:58 +0530

    Add files via upload

---
+++ trackpass.py
@@ -0,0 +1,24 @@
+import random
+import pyautogui
+import string
+
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*<>:'~"
+char_list =list(chars)#conv the same variable to list type
+
+password = pyautogui.password("Enter password: ")
+guess_password =""
+
+
+while(guess_password!=password):
+    guess_password =random.choices(char_list, k=len(password))
+    print("<============"+ str(guess_password) +"=============>")
+
+    if(guess_password == list(password)):
+        print("Cracked Password: "+"".join(guess_password))
+        input()
+        break
+
+
+
+
+
